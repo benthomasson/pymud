eab810f83b33c36e86b0487ed351ab50511b743b
Ben Thomasson <ben.thomasson@gmail.com>
2009-08-25 20:18:01 -0400

    Adding command completion

--- cli.py
+++ cli.py
@@ -3,25 +3,32 @@
 import readline
 import cmd
 
+import sys
 import checker
 import interpreter
 from coroutine import finish
 
 class Mob():
 
-    def __init__(self):
-        self.commands = { "set": self.setVariable }
+    def __init__(self,stdin=sys.stdin,stdout=sys.stdout):
+        self.stdin = stdin
+        self.stdout = stdout
+        self.commands = { "set": self.setVariable,
+                          "say": self.say }
         self.variables = { }
 
     def setVariable(self,name,value):
         self.variables[name] = value
+
+    def say(self, *args):
+        self.stdout.write(" ".join(args) + "\n")
 
 class Cli(cmd.Cmd):
 
     prompt = "pymud>"
 
     def __init__(self,mob):
-        cmd.Cmd.__init__(self)
+        cmd.Cmd.__init__(self,stdin=mob.stdin,stdout=mob.stdout)
         self.mob = mob
 
     def onecmd(self,line):
@@ -30,6 +37,9 @@ class Cli(cmd.Cmd):
             finish(interpreter.interpret(line + "\n",self.mob.commands,self.mob.variables))
         except Exception,e:
             print str(e)
+
+    def completenames(self, *ignored):
+        return self.mob.commands.keys()
 
 if __name__ == '__main__':
     Cli(Mob()).cmdloop()
