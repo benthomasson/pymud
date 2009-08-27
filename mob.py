12de4ff98c05ddd750c69f5448560db87c61e150
Ben Thomasson <ben.thomasson@gmail.com>
2009-08-27 00:16:48 -0400

    Adding chainedmap to mob

--- mob.py
+++ mob.py
@@ -1,27 +1,21 @@
 
-import code
+import sys
+from chainedmap import ChainedMap
 
 class Mob():
 
-    def __init__(self):
-        self._values = {}
-        self._commands = {'__builtins__':None}
-        self._commands['abs'] = abs
+    commands = ChainedMap()
 
-    def __setitem__(self,key,value):
-        self._values[key] = value
-    def __getitem__(self,key):
-        return self._values[key]
+    def __init__(self,stdin=sys.stdin,stdout=sys.stdout):
+        self.stdin = stdin
+        self.stdout = stdout
+        self.variables = {}
 
-    def mobExec(self,code):
-        exec code in self._commands,self
+    def say(self,*args):
+        self.stdout.write(" ".join(args) + "\n")
 
-    def mobEval(self,code):
-        return eval(code,self._commands,self)
+    def setVariable(self,name,value):
+        self.variables[name] = value
 
-    def do(self):
-        print self
-
-    def mobInteract(self):
-        code.InteractiveConsole(self._commands).interact('mob')
-
+Mob.commands.map = {'say':Mob.say,
+                    'set':Mob.setVariable}
