25ae1d0ab3305732ad714a1261510c3867ebfeac
Ben Thomasson <ben.thomasson@gmail.com>
2009-08-27 00:39:03 -0400

    commas not supported yet

--- servertest.py
+++ servertest.py
@@ -8,7 +8,7 @@ def connect():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(('localhost',8000))
     for x in xrange(10):
-        s.send('Hello, world')
+        s.send('Hello world\n')
         data = s.recv(1024)
         print 'Received:', data
     s.close()
