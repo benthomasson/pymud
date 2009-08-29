e67da33e519abe2672e89486c764663d483df65c
Ben Thomasson <ben.thomasson@gmail.com>
2009-08-29 14:55:43 -0400

    Fixing persistence and adding scheduler

--- testserver.py
+++ testserver.py
@@ -3,17 +3,41 @@
 from pymud.mob import Mob
 from pymud.scheduler import Scheduler
 import pymud.chainedmap
-from pymud.persist import P, persist, Persistence
+from pymud.persist import P, Persistence
+import time
 
-persist = Persistence("test.db")
+P.persist = Persistence("test.db")
+print P.persist.id
 
-scheduler = Scheduler()
+if P.persist.exists('scheduler'):
+    scheduler = P.persist.get('scheduler')
+else:
+    scheduler = Scheduler()
+    scheduler.id = 'scheduler'
+    print 'New Scheduler'
+    P.persist.persist(scheduler)
+
+P.persist.sync(100)
+
+print scheduler.id
 
 for x in xrange(100):
     m = Mob()
-    persist.persist(m)
+    P.persist.persist(m)
     print m.id
+    scheduler.schedule(m)
 
+for x in xrange(100):
+    scheduler.run()
 
+try:
+    while True:
+        scheduler.run()
+        time.sleep(0.000000001)
+except KeyboardInterrupt, e:
+    pass
 
+P.persist.sync(100)
+
+P.persist.close()
 
