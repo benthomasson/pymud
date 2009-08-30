21d862fdb15c178a4069da432a4e010540479e61
Ben Thomasson <ben.thomasson@gmail.com>
2009-08-30 12:51:36 -0400

    Adding channel

--- message.py
+++ message.py
@@ -2,6 +2,7 @@
 
 import unittest
 
+from pymud.persist import P
 
 class Message(object):
 
@@ -9,14 +10,55 @@ class Message(object):
         self.type = type
         self.dict = kwargs
 
+class Channel(object):
+    
+    def __init__(self):
+        self.listeners = {}
 
-class Test(unittest.TestCase):
+    def addListener(self,listener):
+        self.listeners[listener.id] = P(listener)
+
+    def removeListener(self,listener):
+        if listener.id in self.listeners:
+            del self.listeners[listener.id] 
+
+    def sendMessage(self,message):
+        for listener in self.listeners.copy().values():
+            if listener(): 
+                listener().receiveMessage(message)
+            else:
+                del self.listeners[listener.id]
+
+
+class TestMessage(unittest.TestCase):
 
     def test(self):
         m = Message("say",message="hello",name="Ed",id="1")
         print "%(name)s says '%(message)s'" % m.dict
 
 
+class TestChannel(unittest.TestCase):
+
+    def receiveMessage(self,message):
+        print "%(name)s says '%(message)s'" % message.dict
+
+    def test(self):
+        self.id = '1'
+        self.deleted = False
+        m = Message("say",message="hello",name="Ed",id="1")
+        c = Channel()
+        c.addListener(self)
+        self.assert_(c.listeners['1'])
+        self.assertEquals(c.listeners['1'].id,'1')
+        self.assertEquals(c.listeners['1'].ref,self)
+        self.assertEquals(c.listeners['1']().id,'1')
+        self.assertEquals(c.listeners['1'](),self)
+        c.sendMessage(m)
+        self.deleted = True
+        c.sendMessage(m)
+
+
+
 if __name__ == "__main__":
     unittest.main()
-    
+
