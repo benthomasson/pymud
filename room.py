742db946241227f74bd1683e39167254719a5bc9
Ben Thomasson <ben.thomasson@gmail.com>
2009-09-01 23:18:18 -0400

    Adding seen and descriptions

--- room.py
+++ room.py
@@ -6,9 +6,15 @@ from pymud.persist import Persistent
 
 class Room(Persistent, Channel, Container):
 
-    def __init__(self):
+    description = "a plain room"
+    detail = "a very plain room"
+
+    def __init__(self,id=None):
         Persistent.__init__(self)
         Channel.__init__(self)
         Container.__init__(self)
+        self.id = id
 
-
+    def seen(self,o):
+        o.sendMessage("look",description=self.description)
+        Container.seen(self,o)
