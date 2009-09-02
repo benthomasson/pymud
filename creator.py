16e4d93fc5a3d912bff2a5c717fcbafca7b5e1fc
Ben Thomasson <ben.thomasson@gmail.com>
2009-09-01 20:53:05 -0400

    Adding goto, fixing message, and commands

--- creator.py
+++ creator.py
@@ -1,26 +1,40 @@
 
 from mob import Mob
+from room import Room
 from pymud.chainedmap import ChainedMap
 from pymud.persist import P
 from pymud.scheduler import Scheduler
-from pymud.message import Message
 from pymud.exceptions import *
+import sys
+import traceback
 
 def create(self,klass):
-    klasses = {"Mob":Mob}
+    klasses = {"Mob":Mob, "Room":Room}
     m = klasses[klass]()
     P.persist.persist(m)
     Scheduler.scheduler.schedule(m)
-    self.sendMessage(Message("created",id=m.id,klass=klass,name=self.id))
+    self.sendMessage("created",id=m.id,klass=klass,name=self.id)
+
+def goto(self,id):
+    newLocation = P.persist.get(id)
+    self.sendMessage("action",description="leave %s" % self.location().__class__.__name__)
+    newLocation.add(self)
+    self.sendMessage("action",description="arrive at %s" % self.location().__class__.__name__)
 
 def shutdown(self):
-    self.sendMessage(Message("say",message="shutdown!",name=self.id))
+    self.sendMessage("say",message="shutdown!",name=self.id)
     raise ShutdownSignal("shutdown!")
 
 class Creator(Mob):
 
     commands = ChainedMap(  parent=Mob.commands,
                             map={   "create":create,
-                                    "shutdown":shutdown})
+                                    "goto":goto,
+                                    "shutdown":shutdown, })
+
+    def __init__(self,*args,**kwargs):
+        Mob.__init__(self,*args,**kwargs)
+        self.commands = ChainedMap(parent=Creator.commands)
+
 
 
