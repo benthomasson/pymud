1baea39b2c38384c1f180264b7e492af10fd5b95
Ben Thomasson <ben.thomasson@gmail.com>
2009-09-01 23:17:41 -0400

    Adding createhere and descriptions

--- creator.py
+++ creator.py
@@ -8,20 +8,33 @@ from pymud.exceptions import *
 import sys
 import traceback
 
-def create(self,klass):
+def create(self,klass,id=None):
+    """Create an instance of a class"""
     klasses = {"Mob":Mob, "Room":Room}
-    m = klasses[klass]()
+    m = klasses[klass](id=id)
     P.persist.persist(m)
     Scheduler.scheduler.schedule(m)
     self.sendMessage("created",id=m.id,klass=klass,name=self.id)
 
+def createHere(self,klass,id=None):
+    """Create an instance of a class in this room"""
+    klasses = {"Mob":Mob, "Room":Room}
+    m = klasses[klass](id=id)
+    P.persist.persist(m)
+    Scheduler.scheduler.schedule(m)
+    if self.location():
+        self.location().add(m)
+    self.sendMessage("created",id=m.id,klass=klass,name=self.id)
+
 def goto(self,id):
+    """Go to another room by id"""
     newLocation = P.persist.get(id)
     self.sendMessage("action",description="leave %s" % self.location().__class__.__name__)
     newLocation.add(self)
     self.sendMessage("action",description="arrive at %s" % self.location().__class__.__name__)
 
 def shutdown(self):
+    """Shutdown the server"""
     self.sendMessage("say",message="shutdown!",name=self.id)
     raise ShutdownSignal("shutdown!")
 
@@ -30,7 +43,10 @@ class Creator(Mob):
     commands = ChainedMap(  parent=Mob.commands,
                             map={   "create":create,
                                     "goto":goto,
+                                    "createhere":createHere,
                                     "shutdown":shutdown, })
+
+    description = "something strange, like a bunch of green glowing letters"
 
     def __init__(self,*args,**kwargs):
         Mob.__init__(self,*args,**kwargs)
