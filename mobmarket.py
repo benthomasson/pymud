b30fe64b48f15a94419623377c082c046e941655
Ben Thomasson <ben.thomasson@gmail.com>
2009-09-03 19:08:34 -0400

    Adding mobmarket

---
+++ mobmarket.py
@@ -0,0 +1,25 @@
+
+from pymud.persist import P,Persistent
+
+class MobMarket(Persistent):
+
+    market = None
+
+    def __init__(self,id = None):
+        Persistent.__init__(self)
+        self.mobs = {}
+        self.id = id
+
+    def add(self,mob):
+        self.mobs[mob.id] = P(mob)
+        
+    def remove(self,mob):
+        if mob.id in self.mobs:
+            del self.mobs[mob.id]
+
+    def getNext(self):
+        if self.mobs:
+            return self.mobs.popitem()[1]
+        else:
+            return None
+
