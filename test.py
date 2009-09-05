5e9c1674f325d5e2f36ebd5c837a56faaf0edd22
Ben Thomasson <ben.thomasson@gmail.com>
2009-09-04 21:04:06 -0400

    Adding all tests to test.py

--- test.py
+++ test.py
@@ -1,1 +1,44 @@
 #!/usr/bin/env python
+
+import unittest
+
+import mob
+import persist
+import chainedmap
+import checker
+import formatter
+import interpreter
+import message
+import scheduler
+import script
+
+class SimpleSuite(unittest.TestSuite):
+
+    def __init__(self,*args,**kwargs):
+        unittest.TestSuite.__init__(self,*args,**kwargs)
+        self.loader = unittest.TestLoader()
+    
+    def addTestsFromTestCase(self,klass):
+        self.addTests(self.loader.loadTestsFromTestCase(klass))
+
+    def runSelf(self):
+        unittest.TextTestRunner(verbosity=2).run(suite)
+
+suite = SimpleSuite()
+map(suite.addTestsFromTestCase,[mob.Test,
+                                persist.TestPersistence,
+                                persist.TestShelve,
+                                persist.TestP,
+                                chainedmap.Test,
+                                checker.Test,
+                                formatter.Test,
+                                interpreter.Test,
+                                message.TestMessage,
+                                message.TestChannel,
+                                scheduler.Test,
+                                script.Test,
+                                ])
+
+
+if __name__ == "__main__":
+   suite.runSelf()
