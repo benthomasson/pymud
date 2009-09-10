5ef71a8bc3d54ba2de2032929143355b8d99eaf3
Ben Thomasson <ben.thomasson@gmail.com>
2009-09-09 22:12:27 -0400

    Adding chat

---
+++ chat.py
@@ -0,0 +1,21 @@
+
+
+from pymud.persist import Persistent
+from pymud.message import Channel
+
+
+class ChatRoom(Channel,Persistent):
+
+    def __init__(self,name,id):
+        Channel.__init__(self)
+        Persistent.__init__(self)
+        self.id = id
+        self.name = name
+
+    def addListener(self,listener):
+        Channel.addListener(self,listener)
+        self.sendMessage("chatjoin",name=listener.id,channel=self.name)
+
+    def removeListener(self,listener):
+        self.sendMessage("chatleave",name=listener.id,channel=self.name)
+        Channel.removeListener(self,listener)
