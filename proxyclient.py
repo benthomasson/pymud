3c2901ceccab82b078530f125270aae9b01a5e15
Ben Thomasson <ben.thomasson@gmail.com>
2009-11-17 19:40:37 -0500

    Adding ProxyPersistence and multiprocess tests

--- proxyclient.py
+++ proxyclient.py
@@ -1,9 +1,65 @@
+#!/usr/bin/env python
 
-from multiprocessing.managers import BaseManager
+from multiprocessing.managers import BaseManager, Process
+from threading import Thread
 from proxy import SimProxy
+from pymud.persist import P, Persistence, getP
+import time
+
+import unittest
+from testfixture import RoomTestFixture
 
 class ProxyManager(BaseManager): pass
 
 ProxyManager.register('get_sim',proxytype=SimProxy)
 
+class ProxyPersistence(object):
 
+    manager = None
+
+    def __init__(self):
+        pass
+
+    def get(self,id):
+        return self.manager.get_sim(id)
+
+class Test(RoomTestFixture):
+
+    def startServer(self):
+        print 'startServer'
+        import proxyserver
+        from pymud.mob import Mob
+        mob = self.createHere("mob",Mob)
+        m2 = P.persist.get("mob")
+        self.assert_(mob is m2)
+        print mob,repr(mob)
+        print P.persist
+        proxyserver.startServer(ThreadOrProcess=None)
+
+    def test(self):
+        from pymud.mob import Mob
+        thread = Process(target=self.startServer)
+        thread.daemon = True
+        thread.start()
+        #thread.join()
+        time.sleep(1)
+        #self.startServer()
+        client = ProxyManager(address=("",6000),authkey="abc")
+        ProxyPersistence.manager = client
+        P.persist = ProxyPersistence()
+        client.connect()
+        print P.persist
+        s = client.get_sim("mob")
+        self.assertEquals(s.description,'an ugly son of a mob')
+        print s,repr(s)
+        loc = s.location
+        self.assertFalse(loc.ref)
+        self.assertEquals(loc.id,'world')
+        print repr(s.location)
+        print repr(s.location())
+        print str(s.location)
+        print str(s.location())
+
+
+if __name__ == "__main__":
+    unittest.main()
--- proxyserver.py
+++ proxyserver.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import unittest
-from multiprocessing.managers import BaseManager
+from multiprocessing.managers import BaseManager, Process
 from threading import Thread
 from sim import Sim
 from pymud.persist import P, Persistence, getP
@@ -16,11 +16,17 @@ class ProxyManager(BaseManager): pass
 
 ProxyManager.register('get_sim',get_sim,proxytype=SimProxy)
 
-def startServer(address=("",6000)):
+def startServer(address=("",6000),ThreadOrProcess=Thread):
     manager = ProxyManager(address=address,authkey='abc')
     server = manager.get_server()
-    thread = Thread(target=server.serve_forever)
-    thread.start()
+    if ThreadOrProcess:
+        thread = ThreadOrProcess(target=server.serve_forever)
+        if ThreadOrProcess is Process:
+            thread.daemon = True
+        thread.start()
+        return thread
+    else:
+        server.serve_forever()
 
 class Test(RoomTestFixture):
 
@@ -30,7 +36,7 @@ class Test(RoomTestFixture):
         m2 = P.persist.get("mob")
         self.assert_(mob is m2)
         import proxyclient
-        startServer()
+        startServer(ThreadOrProcess=Process)
         client = proxyclient.ProxyManager(address=("localhost",6000),authkey="abc")
         client.connect()
         s = client.get_sim("mob")
@@ -41,7 +47,7 @@ class Test(RoomTestFixture):
         self.assertEquals(loc.id,'world')
         print repr(s.location)
         print repr(s.location())
-        
 
 if __name__ == "__main__":
     unittest.main()
+
