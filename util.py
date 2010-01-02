1b21117914b6ab4c0d4ee8b9d51206b85c7a2841
Ben Thomasson <ben.thomasson@gmail.com>
2010-01-01 21:07:33 -0500

    Adding utilities

--- util.py
+++ util.py
@@ -1,3 +1,4 @@
+from pymud.exceptions import *
 
 def classname(obj):
     return obj.__class__.__name__
@@ -8,4 +9,16 @@ def getAllSubclasses(klass):
     for x in klass.__subclasses__():
         klasses += getAllSubclasses(x)
     return list(set(klasses))
+
+def checkVoid(self,message):
+    if not self.location():
+        raise GameException("You are in the void. %s" % message)
+
+def getFirstTarget(self,target,exception):
+    print target
+    target = self.location().get(attribute=target)
+    print target
+    if target:
+        return target[0]
+    raise GameException(exception)
 
